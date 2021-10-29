1384cefc7b5e6e6f0e3b41ad86b8948f8554117d
Marco <marco.zappani@gmail.com>
2021-10-29 19:01:54 +0200

    defualt video model and crud created

--- common/models/Video.php
+++ common/models/Video.php
@@ -3,7 +3,6 @@
 namespace common\models;
 
 use Yii;
-use yii\helpers\FileHelper;
 
 /**
  * This is the model class for table "{{%video}}".
@@ -23,12 +22,6 @@ use yii\helpers\FileHelper;
  */
 class Video extends \yii\db\ActiveRecord
 {
-    /**
-     * @var \yii\web\UploadedFile
-     */
-
-    public $video;
-
     /**
      * {@inheritdoc}
      */
@@ -89,27 +82,5 @@ class Video extends \yii\db\ActiveRecord
     public static function find()
     {
         return new \common\models\query\VideoQuery(get_called_class());
-    }
-
-    public function save($runValidation = true, $attributeNames = null)
-    {
-        $isInsert = $this->isNewRecord;
-        if ($isInsert) {
-            $this->video_id = Yii::$app->security->generateRandomString(length: 8);
-            $this->title = $this->video->name;
-            $this->video_name = $this->video->name;
-        }
-        $saved = parent::save($runValidation, $attributeNames);
-        if (!$saved) {
-            return false;
-        }
-        if ($isInsert) {
-            $videoPath = Yii::getAlias(alias: '@frontend/web/storage/videos/' . $this->video_id . 'mp4');
-            if (!is_dir(dirname($videoPath))) {
-                FileHelper::createDirectory(dirname($videoPath));
-            }
-            $this->video->saveAs($videoPath);
-        }
-        return true;
     }
 }
